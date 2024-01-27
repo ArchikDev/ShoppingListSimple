94ad1f6c36d3f512e46b1e704a67a5985cb6a979
a.galeev <a.galeev@malltech-lab.ru>
2024-01-27 22:15:25 +0500

    add screens

---
+++ app/src/main/java/ru/ar4uk/shoppinglistsimple/presentation/settings_screen/SettingsScreen.kt
@@ -0,0 +1,19 @@
+package ru.ar4uk.shoppinglistsimple.presentation.shopping_list_screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SettingsScreen() {
+    Text(
+        text = "SettingsScreen",
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentHeight()
+            .wrapContentWidth()
+    )
+}
