e439b09c0beafd6dd816ad3aabce3c8a9e100e18
a.galeev <a.galeev@malltech-lab.ru>
2024-02-09 16:21:09 +0500

    add SettingsViewModel

---
+++ app/src/main/java/ru/ar4uk/shoppinglistsimple/presentation/settings_screen/ColorUtils.kt
@@ -0,0 +1,16 @@
+package ru.ar4uk.shoppinglistsimple.presentation.settings_screen
+
+object ColorUtils {
+    val colorList = listOf(
+        "#2480FF",
+        "#AC24C4",
+        "#24C467",
+        "#24C4AC",
+        "#C49424",
+        "#C42424",
+        "#55533B",
+        "#3B554B",
+        "#55423B",
+        "#553B3E",
+    )
+}
